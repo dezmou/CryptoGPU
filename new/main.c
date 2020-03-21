33d4af5579c6b3ab4587ae0a7dd773fc6eb21f5a
Dezmou <incredibledez@gmail.com>
2020-03-21 01:15:44 +0000

    testing new stuff

--- new/main.c
+++ new/main.c
@@ -13,6 +13,12 @@
 #define KCYN "\x1B[36m"
 #define KWHT "\x1B[37m"
 
+#define RAKE 0.00075
+// #define RAKE 0
+
+#define BUY 1
+#define SELL 2
+
 typedef struct {
     long time;
     double open;
@@ -25,8 +31,8 @@ typedef struct {
 Minute *data;
 int nbrMinutes;
 int cursor;
-double ok = 0;
-double ko = 0;
+
+double bank = 10000;
 
 void fillData() {
     int fd = open("BTCUSDT", O_RDONLY);
@@ -38,33 +44,69 @@ void fillData() {
     nbrMinutes = size / sizeof(Minute);
 }
 
-void printLine(Minute *minute) {
-    // printf("%ld %lf %lf %lf %lf %lf \n", minute->time, minute->open,
-    // minute->high, minute->low, minute->close, minute->volume);
-    double change = 100000 - (minute->open / minute->close * 100000);
-    // printf("%lf %lf %lf\n",minute->open, minute->close, change);
-    if (abs(change) > 800) {
-        double changeNext = 100000 - (minute->open / minute[100].open * 100000);
-        if ((change > 0 && changeNext > 0) || (change < 0 && changeNext < 0)) {
-            ok += abs(changeNext);
-            printf(KGRN);
-        } else {
-            ko += abs(changeNext);
-            printf(KRED);
+// void printLine(Minute *minute) {
+//     // printf("%ld %lf %lf %lf %lf %lf \n", minute->time, minute->open,
+//     // minute->high, minute->low, minute->close, minute->volume);
+//     double change = 100000 - (minute->open / minute->close * 100000);
+//     // printf("%lf %lf %lf\n",minute->open, minute->close, change);
+//     if (abs(change) > 800) {
+//         nbrBet++;
+//         double changeNext = 100000 - (minute->open / minute[5].open *
+//         100000); if ((change > 0 && changeNext > 0) || (change < 0 &&
+//         changeNext < 0)) {
+//             ok += abs(changeNext);
+//             // ok -= abs(changeNext * 0.02);
+//             printf(KGRN);
+//         } else {
+//             ko += abs(changeNext);
+//             printf(KRED);
+//         }
+//         printf(
+//             "%ld  H-L:%11lf  O-C:%11lf   VO:%11lf   CH:%11lf  "
+//             "CHN:%11lf  OK:%11lf   KO:%11lf  DIF:%11lf  BET: %d\n",
+//             minute->time, minute->high - minute->low,
+//             minute->open - minute->close, minute->volume, change / 1000,
+//             changeNext / 1000, ok / 1000, ko / 1000, (ok / ko), nbrBet);
+//     }
+// }
+
+double bet(Minute *minute, char type, double amount, double closeWin,
+           double closeLoss) {
+    double fee = amount * RAKE;
+    double final = amount;
+    for (int i = 0; i < 60; i++) {
+        double change = ((minute[i].close / minute->close * 100) - 100) *
+                        (type == BUY ? 1 : -1);
+        if (change > closeWin || change < -closeLoss) {
+            final = amount + (amount * (change * 0.01));
+            if (change < 0) {
+                printf(KRED);
+            } else {
+                printf(KGRN);
+            }
+            printf("%7d |  %lf %lf change %lf final %lf %s bank : %lf\n", i,
+                   minute->close, minute[i].close, change, final,
+                   type == BUY ? "BUY" : "SELL", bank);
+            break;
         }
-        printf(
-            "%ld    H-L:%11lf    O-C:%11lf    VO:%11lf      CH:%11lf       "
-            "CHN:%11lf     OK:%11lf    KO:%11lf\n",
-            minute->time, minute->high - minute->low,
-            minute->open - minute->close, minute->volume, change / 1000,
-            changeNext / 1000, ok / 1000, ko / 1000);
     }
+    fee += final * RAKE;
+    final += -fee;
+    return final;
+}
+
+void play(Minute *minute) {
+    double betSize = 100;
+    bank += -betSize;
+    bank += bet(minute, SELL, betSize, 1.3, 1.3);
+    // printf(" bank : %lf\n", bank);
 }
 
 int main() {
     fillData();
-    for (cursor = 0; cursor < nbrMinutes; cursor++) {
-        printLine(&data[cursor]);
+    for (cursor = 0; cursor < nbrMinutes; cursor += 5000) {
+        play(&data[cursor]);
+        // getchar();
         // break;
     }
     return 0;
